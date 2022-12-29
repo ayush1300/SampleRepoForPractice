272fe71cc74ca798b91dc6bafabbfa4ab11a0e9d
Ayush Shukla <ayshukla@amazon.com>
2022-12-30 00:35:10 +0530

    Subtraction added

--- Sum.java
+++ Sum.java
@@ -1,7 +1,10 @@
 class Sum {
    public static void main(String[] args) {
       int a = 5,b = 9;
+      int d = 15, e = 19;
       int c = a+b;
+      int f = e-d;
       System.out.println(c);
+      System.out.println(f);
    }
 }
